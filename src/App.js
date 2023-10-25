08336b2fccefa657eab8e53a67e6f4c23db1628a
Ubuntu <ubuntu@ip-172-31-42-110.ec2.internal>
2023-10-25 10:19:57 +0000

    hi

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import './App.css';
-import { Route, Routes } from "react-router-dom";
-import Header from './componets/Header';
-import React from 'react';
-import Login from './componets/Login';
-import Blogs from './componets/Blogs';
-import UserBlogs from './componets/UserBlogs'
-import AddBlogs from './componets/AddBlogs'
-import BlogDetail from './componets/BlogDetail'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/common/Navbar";
+import Home from "./components/pages/Home";
+import CrudAdd from "./components/cruds/CrudAdd";
+import CrudTable from "./components/cruds/CrudTable";
+import CrudListView from "./components/cruds/CrudListView";
+import CrudGridView from "./components/cruds/CrudGridView";
+import CrudDetails from "./components/cruds/CrudDetails";
+import CrudEdit from "./components/cruds/CrudEdit";
+import CrudDelete from "./components/cruds/CrudDelete";
+import Footer from "./components/common/Footer";
 
 function App() {
-  return <React.Fragment>
-    <header>
-      <Header/>
-    </header>
-    <main>
-    <Routes>
-      <Route path="/login" element={<Login/>}></Route>
-      <Route path="/blogs" element={<Blogs/>}></Route>
-      <Route path="/myBlogs" element={<UserBlogs/>}></Route>
-      <Route path="/myBlogs/:id" element={<BlogDetail/>}></Route>
-      <Route path="/blogs/add" element={<AddBlogs />} />
-    </Routes>
-    </main>
+	return (
+		<div className="App">
+			<Router>
+				<Navbar />
 
-  </React.Fragment>;
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/api/" element={<Home />} />
+					<Route exact path="/cruds" element={<CrudTable />} />					
+					<Route exact path="/cruds/list-view" element={<CrudListView />} />
+					<Route exact path="/cruds/grid-view" element={<CrudGridView />} />
+					<Route exact path="/cruds/new" element={<CrudAdd />} />
+					<Route exact path="/cruds/:_id" element={<CrudDetails />} />
+					<Route exact path="/cruds/:_id/edit" element={<CrudEdit />} />
+					<Route exact path="/cruds/:_id/delete" element={<CrudDelete />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
